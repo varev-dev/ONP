16dd499b17d5730a9d9d2ba2ab8c1739e62fecbf
Kacper Doga <doga.kacper@gmail.com>
2024-03-24 21:03:44 +0100

    handle parenthesses (recursive call), parse to RPN

--- sources/Reader.c
+++ sources/Reader.c
@@ -5,51 +5,62 @@
 #include <stdio.h>
 
 int compare_significance(Node* node, char* operand) {
-    return significance(node->data->value) > significance(operand);
+    char signify = significance(node->data->value), signify2 = significance(operand);
+
+    if (signify > signify2)
+        return 1;
+    if (signify == signify2)
+        return 0;
+    return -1;
 }
 
-void append_negations(Stack* main, Stack* operands) {
+void append_negations(Stack* main, Stack* operators) {
     Node* node;
-    while(operands->first != NULL && *operands->first->data->value == 'N') {
-        node = pop(operands, FIRST);
+    while(operators->first != NULL && *operators->first->data->value == 'N') {
+        node = pop(operators, FIRST);
         push(main, node, LAST);
     }
     node = NULL;
 }
 
-/**
- * @todo operand comparison & appending to top of the stack
- */
+void append_operators(Stack* main, Stack* operators, char* operator) {
+    if (compare_significance(operators->last, operator) == 0)
+        append_stack(main, operators);
+    else if (compare_significance(operators->first, operator) != -1)
+        push(main, pop(operators,FIRST), LAST);
+}
+
 Stack* read_input(Tag endTag) {
-    Stack* main = init_stack(NULL), * operands = init_stack(NULL);
+    Stack* main = init_stack(NULL), * operators = init_stack(NULL);
     Token* token = NULL;
     char input = '\0';
 
-    while (input != endTag) {
-        print_stack(main);
+    while (1) {
         scanf("%c", &input);
+
         if (input == DIVIDER) {
-            push(main, init_node(token, NULL), LAST);
-            if (token->type != OPERAND) append_negations(main, operands);
+            if (token && token->type != OPERATOR) {
+                push(main, init_node(token, NULL), LAST);
+                append_negations(main, operators);
+            }
             token = NULL;
         } else if (input >= '0' && input <= '9') {
             if (token == NULL) token = init_token_with_value(NUMBER, &input);
             else append_to_token(token, &input);
+        } else if (input == '(') {
+            append_stack(main, read_input(CLOSE));
         } else if (significance(&input)) {
-            token = init_token_with_value(OPERAND, &input);
+            token = init_token_with_value(OPERATOR, &input);
 
-            if (operands->first == NULL) {
-                push(operands, init_node(token, NULL), FIRST);
-                continue;
-            }
+            if (operators->first != NULL && *operators->first->data->value != 'N')
+                append_operators(main, operators, &input);
 
-            if (compare_significance(operands->first, &input)) {
-                Node* node = pop(operands,FIRST);
-                push(main, node, LAST);
-            }
+            push(operators, init_node(token, NULL), FIRST);
+        } else if (input == endTag) {
+            append_stack(main, operators);
+            break;
         }
     }
-
     return main;
 }
 
