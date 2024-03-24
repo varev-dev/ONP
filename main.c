7bc9d42e13bc828e607cfb0af6413f466b62ca67
Kacper Doga <doga.kacper@gmail.com>
2024-03-24 21:04:26 +0100

    visualize basic usage of a Reader function

--- main.c
+++ main.c
@@ -1,24 +1,8 @@
-#include "includes/Stack.h"
+#include "includes/Reader.h"
 #include <string.h>
 
 int main() {
-    Stack* stack = init_stack(NULL);
-    char* text = malloc(16 * sizeof(char));
-    strcpy(text, "8321");
-    push(stack, init_node(init_token_with_value(NUMBER, text), NULL));
-
-    text = malloc(16 * sizeof(char));
-    strcpy(text, "+");
-    push(stack, init_node(init_token_with_value(OPERAND, text), NULL));
-
-    text = malloc(16 * sizeof(char));
-    strcpy(text, "MIN1");
-    push(stack, init_node(init_token_with_value(FUNCTION, text), NULL));
-
-    print_stack(stack);
-
-    pop(stack, 0);
-
+    Stack* stack = read_input(END);
     print_stack(stack);
     delete_stack(stack);
     return 0;
