9e4a62fc04f97994adcc3d848e1cf1392734e9e6
Kacper Doga <doga.kacper@gmail.com>
2024-03-25 15:48:32 +0100

    handle N expressions instead of a single one

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include "includes/Reader.h"
-#include <string.h>
+#include <stdio.h>
 
 int main() {
-    Stack* stack = read_input(END, 0);
-    print_stack(stack);
-    delete_stack(stack);
+    unsigned short expressions;
+    scanf("%hd", &expressions);
+
+    while (expressions > 0) {
+        Stack* stack = read_input(END, 0);
+        print_stack(stack);
+        delete_stack(stack);
+        expressions--;
+    }
+
     return 0;
 }
